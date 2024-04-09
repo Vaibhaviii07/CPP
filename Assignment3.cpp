ba3c725ec4505a119b2189b6f1671445debc7527
Vaibhavi Tingane <123562096+Vaibhaviii07@users.noreply.github.com>
2024-04-09 16:11:32 +0530

    Add files via upload

---
+++ Assignment3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char  a;
+    cout<<"Enter any charcater :";
+    cin>>a;
+    int value=(int)a;
+
+    cout<<"ASCII equivalent is "<<a<<"="<<value<<endl;
+}
