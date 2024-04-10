2ae2de3e48786991772ff104359a384ccbccf882
Vaibhavi Tingane <123562096+Vaibhaviii07@users.noreply.github.com>
2024-04-10 22:19:39 +0530

    Add files via upload

---
+++ Assignment4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter any two numbers: ";
+    cin>>a>>b;
+    float add = a+b;
+    float sub = a-b;
+    float mul = a*b;
+    float div = a/b;
+    cout<<"sum of the two numbers are :"<<add<<endl;
+    cout<<"Difference of the two numbers are :"<<sub<<endl;
+    cout<<"product of the two number are :"<<mul<<endl;
+    cout<<"quoitent of the two number :"<<div<<endl;
+
+}
